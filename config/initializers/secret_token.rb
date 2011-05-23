f3c2b2cf594313bd0bad3b89b16b1114b5f5833f
unknown <Rob@.(none)>
2011-05-23 17:20:29 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveMSRD::Application.config.secret_token = '983104d688042e0c0943b20961c7433b2b6e948ddb7729bc314c4ec984062aed2fe5d245d23da031f3f3f0f01dc485c9d985ec03c7ffbe08a5acfd21faf18f29'
